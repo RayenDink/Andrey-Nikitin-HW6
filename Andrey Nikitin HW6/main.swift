eba3eb6095c82e661845aa158f148c8359230f98
Rayen <zufirka@icloud.com>
2020-07-21 00:38:00 +0300

    Initial Commit

---
+++ Andrey Nikitin HW6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Andrey Nikitin HW6
+//
+//  Created by Rayen on 7/21/20.
+//  Copyright © 2020 Rayen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
